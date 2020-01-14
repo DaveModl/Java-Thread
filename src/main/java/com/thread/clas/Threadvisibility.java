7a8539b02eb1bdcf161274bad86b692e1902a6e9
Dave <Davemo-coderpersonal@hotmail.com>
2020-01-14 16:22:22 +0800

    add 线程可见性

--- src/main/java/com/thread/clas/Threadvisibility.java
+++ src/main/java/com/thread/clas/Threadvisibility.java
@@ -1,4 +1,33 @@
 package com.thread.clas;
 
 public class Threadvisibility {
+    int i = 0;
+    boolean isRunning = true;
+
+    public static void main(String[] args) throws InterruptedException {
+        Threadvisibility threadvisibility = new Threadvisibility();
+        new Thread(() ->{
+            System.out.println("hi I'm in");
+            /**
+             * 子线程运行代码，读取值
+             *可能没有读到
+             */
+           while(threadvisibility.isRunning){
+               threadvisibility.i++;
+           }
+            System.out.println(threadvisibility.i);
+        }).start();
+
+        Thread.sleep(3000L);
+        /**
+         * 主线程写入值
+         * 1.在写内存之前会先写缓存（可见性问题，写入读不到）
+         * 缓存再同步到内存
+         * （缓存同步协议，保证能够读到，所以不是由于cpu缓存导致的）
+         * 可能没有写入
+         * 2.指令重排序
+         */
+        threadvisibility.isRunning = false;
+        System.out.println("shutdown...");
+    }
 }
